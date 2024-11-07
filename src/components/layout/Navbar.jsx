f9192c62b07e87aaf5281f633fc367c235259cc3
Mustafa Panjiwala <56435815+mustafapanjiwala@users.noreply.github.com>
2024-11-07 19:30:48 +0530

    logo fixed

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/Logo.png";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
